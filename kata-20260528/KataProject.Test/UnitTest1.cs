296ad22d8c38356d008895870b2b26730da23783
Carlos Da Silva <carloshdezdasilva@gmail.com>
2026-05-28 08:22:28 +0100

    chore: Create empty solution and projects

---
+++ kata-20260528/KataProject.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace KataProject.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Charlie();
+    }
+}
---
+++ kata-20260528/KataProject/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace KataProject;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
